59f67686141eb06dc6fb979b76d9906a008fa16a
Piotr Merski <merskip@gmail.com>
2018-12-07 21:45:39 +0100

    Refactoring archiving pair credentials

---
+++ src/main/kotlin/pl/merskip/homekitcollector/archive/GsonByteArrayTypeAdapter.kt
@@ -0,0 +1,16 @@
+package pl.merskip.homekitcollector.archive
+
+import com.google.gson.*
+import java.lang.reflect.Type
+import java.util.*
+
+class GsonByteArrayTypeAdapter : JsonSerializer<ByteArray>, JsonDeserializer<ByteArray> {
+
+    override fun serialize(src: ByteArray?, typeOfSrc: Type?, context: JsonSerializationContext?): JsonElement {
+        return JsonPrimitive(Base64.getEncoder().withoutPadding().encodeToString(src))
+    }
+
+    override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): ByteArray {
+        return Base64.getDecoder().decode(json?.asString)
+    }
+}
--- src/main/kotlin/pl/merskip/homekitcollector/main.kt
+++ src/main/kotlin/pl/merskip/homekitcollector/main.kt
@@ -1,39 +1,18 @@
 package pl.merskip.homekitcollector
 
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import pl.merskip.homekitcollector.pairing.PairCredentials
+import pl.merskip.homekitcollector.archive.Archiver
 import pl.merskip.homekitcollector.pairing.PairSetup
 import pl.merskip.homekitcollector.pairing.PairVerify
 import java.io.File
 
 
-fun main(args : Array<String>) {
+fun main(args: Array<String>) {
 
-    val credentials: PairCredentials
-
-    val pairCredentialsFile = File("pair_credentials.json")
-    if (pairCredentialsFile.exists()) {
-        println("Read credentials from file: ${pairCredentialsFile.absolutePath}")
-        credentials = Gson().fromJson(String(pairCredentialsFile.readBytes()), PairCredentials::class.java)
-    }
-    else {
-
-        credentials = PairSetup("192.168.1.8", 51826)
+    val pairCredentials = Archiver.require(File("pair_credentials.json")) {
+        PairSetup("192.168.1.8", 51826)
                 .pair("031-45-154")
-
-        val credentialsJson = GsonBuilder().setPrettyPrinting().create().toJson(credentials)
-        val writer = pairCredentialsFile.printWriter()
-        writer.write(credentialsJson)
-        writer.close()
-
-        println("Saved credentials to file: ${pairCredentialsFile.absolutePath}")
     }
 
-    println(credentials)
-
-    println("Pair verification...")
-    PairVerify(credentials).verify()
-    println("Done :-)")
+    PairVerify(pairCredentials).verify()
 
 }
